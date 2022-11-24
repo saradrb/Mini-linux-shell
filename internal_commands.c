93ee6e3d299e0d2c8a3eda821e373b4cc5664b49
Celine Ye <celine.ye20@gmail.com>
2022-11-24 21:49:50 +0100

    Finishes commentary for exit and cd

--- internal_commands.c
+++ internal_commands.c
@@ -4,6 +4,10 @@ char previous_rep[PATH_MAX] = {'\0'};
 char current_rep[PATH_MAX] = {'\0'};
 int previous_return_value = 0;
 
+/* Exit the program with a certain value. If no value has been given, close
+ * the program with the value the last function called has returned, otherwise
+ * close it with the given value.
+ * Return 1 if the given argument isn't valid (the program hasn't been closed) */
 int my_exit(char **arguments, int length) {
   if (length > 1) {
     write(STDOUT_FILENO, "exit: Too many arguments\n", 26);
@@ -25,9 +29,8 @@ int my_exit(char **arguments, int length) {
   }
 }
 
-/* Search a directory in path between path[pos_path]
- * and the first next '/' or the end of path
- */
+/* Search a directory in path between path[pos_path] and the first next '/' or
+ * the end of path */
 static char *directory(char *path, int pos_path, int len) {
   // allocation and management of the memory
   char *word = malloc(sizeof(char) * (len - pos_path + 2));
@@ -49,9 +52,7 @@ static char *directory(char *path, int pos_path, int len) {
   return word;
 }
 
-/* Suppress a directory at the end of path
- * (between path[pos] and a char '/')
- */
+/* Suppress a directory at the end of path (between path[pos] and a char '/') */
 static int suppr_directory(char *path, int pos) {
   do {
     path[pos] = '\0';
@@ -60,9 +61,8 @@ static int suppr_directory(char *path, int pos) {
   return pos;
 }
 
-/* Take an absolute path and return a simplification
- * of this path without the . and .. directory
- */
+/* Take an absolute path and return a simplification of this path without the
+ * . and .. directory */
 static char *get_final_path(char *path) {
   // verify if the first character is '/' (root)
   if (path[0] != '/') {
@@ -166,8 +166,7 @@ static int is_valid(char **arguments, int length, bool is_cd, char *option,
 }
 
 /* Return the absolute path interpreted logically if there is no option or if
- * the option is -L, or interpreted physicaly if the option is -P
- */
+ * the option is -L, or interpreted physicaly if the option is -P */
 int my_pwd(char **arguments, int length) {
   char option[3] = {'\0'};
   // check if the arguments are valid
@@ -201,6 +200,14 @@ int my_pwd(char **arguments, int length) {
   return 1;
 }
 
+/* Change the current working directory to another one. If no path is given,
+ * change directory to the one described in 'HOME', if the path given is '-',
+ * change directory to the previous one, and if a path is given, change
+ * directory to the one described in path.
+ * If no option is given, or if the option given is '-L', interprete the given
+ * path logically. If the option given is '-P', interprete it physically.
+ * Return 0 if the directory has been successfully changed and 1 otherwise.
+*/
 int my_cd(char **arguments, int length) {
   char option[3] = {'\0'};
   char path[PATH_MAX] = {'\0'};
--- internal_commands.h
+++ internal_commands.h
@@ -11,11 +11,24 @@
 #define MAX_ARGS_STRLEN 4096
 #define PATH_MAX 4096
 
+/* Exit the program with a certain value. If no value has been given, close
+ * the program with the value the last function called has returned, otherwise
+ * close it with the given value.
+ * Return 1 if the given argument isn't valid (the program hasn't been closed)
+*/
+int my_exit(char **arguments, int length);
+
 /* Return the absolute path interpreted logically if there is no option or if
  * the option is -L, or interpreted physicaly if the option is -P
  */
 int my_pwd(char **arguments, int length);
 
+/* Change the current working directory to another one. If no path is given,
+ * change directory to the one described in 'HOME', if the path given is '-',
+ * change directory to the previous one, and if a path is given, change
+ * directory to the one described in path.
+ * If no option is given, or if the option given is '-L', interprete the given
+ * path logically. If the option given is '-P', interprete it physically.
+ * Return 0 if the directory has been successfully changed and 1 otherwise.
+*/
 int my_cd(char **arguments, int length);
-
-int my_exit(char **arguments, int length);
